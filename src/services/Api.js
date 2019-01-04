4f60d4324fb652361754f4a0c0d104404cb7e0af
Vlad <vladislavmoiseenkov@gmail.com>
2019-01-04 16:12:16 +0200

    + add: procfile

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store'
 
 export default () => {
   return axios.create({
-    baseURL: 'https://notes-app-vue-server.herokuapp.com:8081',
+    baseURL: 'https://notes-app-vue-server.herokuapp.com',
     headers: {
       Authorization: `Bearer ${store.state.auth.token}`
     }
